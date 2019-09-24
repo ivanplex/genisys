252e41fc2196a679910d1aac44aef180b786cb83
Ivan Chan <ivan@ivanplex.com>
2019-09-24 16:20:28 +0100

    admin object control

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-# from shop.atomic.models import AtomicComponent
-#
-# admin.site.register(AtomicComponent)
+from shop.atomic.models import AtomicComponent, AtomicSpecification, AtomicPrerequisite
+from shop.assembly.models import Blueprint, Product, ProductSpecification, ProductPrerequisite
+
+admin.site.register(AtomicComponent)
+admin.site.register(AtomicPrerequisite)
+admin.site.register(AtomicSpecification)
+admin.site.register(Blueprint)
+admin.site.register(Product)
+admin.site.register(ProductSpecification)
+admin.site.register(ProductPrerequisite)
