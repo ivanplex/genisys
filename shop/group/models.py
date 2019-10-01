30e94e85ca7d1e014fc0f95e14eae16a84c75762
Ivan Chan <ivan@ivanplex.com>
2019-10-01 15:16:00 +0100

    Change concept. AtomicGroup and ProductGroup seperation

--- shop/group/models.py
+++ shop/group/models.py
@@ -2,14 +2,17 @@ from django.db import models
 # from django.contrib.contenttypes.fields import GenericForeignKey
 # from django.contrib.contenttypes.models import ContentType
 from shop.models import TimestampedModel
+from shop.atomic.models import AtomicComponent
+from shop.assembly.models import Product
 
 
-class Group(TimestampedModel):
+class AtomicGroup(TimestampedModel):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.CharField(max_length=100)
+    members = models.ManyToManyField(AtomicComponent, related_name='members')
 
-    # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    # object_id = models.PositiveIntegerField()
-    # content_object = GenericForeignKey('content_type', 'object_id')
 
-    members = models.ForeignKey(TimestampedModel, on_delete=models.CASCADE, related_name='members')
+class ProductGroup(TimestampedModel):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=100)
+    members = models.ManyToManyField(Product, related_name='members')
