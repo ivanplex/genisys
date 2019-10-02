2ea68a4ded21131ccd6e82b9acedcc970a94513f
Ivan Chan <ivan@ivanplex.com>
2019-10-02 11:10:19 +0100

    Import Atomic Attribute

--- shop/scripts/endfitting_import.py
+++ shop/scripts/endfitting_import.py
@@ -1,5 +1,5 @@
 import csv
-from shop.atomic.models import AtomicComponent
+from shop.atomic.models import AtomicComponent, AtomicAttribute
 
 
 def run():
@@ -7,8 +7,20 @@ def run():
         csvreader = csv.reader(csvfile, delimiter=',')
         for row in csvreader:
             print(', '.join(row))
-            AtomicComponent.objects.create(
+            atom = AtomicComponent.objects.get_or_create(
                 stock_code=row[3],
                 part_code=row[2],
-                warehouse_location=row[4]
+                warehouse_location=row[4],
+                type=row[0]
             )
+            AtomicAttribute.objects.get_or_create(
+                atomic_component=atom[0],
+                key='Thread size',
+                value=row[9]
+            )
+            AtomicAttribute.objects.get_or_create(
+                atomic_component=atom[0],
+                key='Outer Comp.Factor',
+                value=row[11]
+            )
+
