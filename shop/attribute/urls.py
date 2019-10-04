1036b3b0b536e126e5b049a1e1cad59426516710
Ivan Chan <ivan@ivanplex.com>
2019-10-04 12:48:25 +0100

    Product Attribute URL

--- shop/attribute/urls.py
+++ shop/attribute/urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls import url
 
 from shop.assembly.api import (
+    #Blueprint Attribute
     BlueprintAttributeList,
     BlueprintAttributeDetails,
     BlueprintAttributeCreate,
     BlueprintAttributeUpdate,
     BlueprintAttributeDestroy,
+    # Product Attribute
+    ProductAttributeList,
+    ProductAttributeDetails,
+    ProductAttributeCreate,
+    ProductAttributeUpdate,
+    ProductAttributeDestroy,
 )
 
 namespace_prefix = "shop.attribute."
 
 urlpatterns = [
-    # Blueprint
+    # Blueprint Attribute
     url(r'^blueprint/$', BlueprintAttributeList.as_view(),
         name=namespace_prefix + "blueprint.list"),
     url(r'^blueprint/view/(?P<pk>\d+)/$', BlueprintAttributeDetails.as_view(),
@@ -22,15 +29,15 @@ urlpatterns = [
         name=namespace_prefix + "component.update"),
     url(r'^blueprint/delete/(?P<pk>\d+)/$', BlueprintAttributeDestroy.as_view(),
         name=namespace_prefix + "component.destroy"),
-    # Product
-    # url(r'^product/$', ProductList.as_view(),
-    #     name=namespace_prefix + "product.list"),
-    # url(r'^product/view/(?P<pk>\d+)/$', ProductDetails.as_view(),
-    #     name=namespace_prefix + "product.detail"),
-    # url(r'^product/create/', ProductCreate.as_view(),
-    #     name=namespace_prefix + "product.create"),
-    # url(r'^product/update/(?P<pk>\d+)/$', ProductUpdate.as_view(),
-    #     name=namespace_prefix + "product.update"),
-    # url(r'^product/delete/(?P<pk>\d+)/$', ProductDestroy.as_view(),
-    #     name=namespace_prefix + "product.destroy"),
+    # Product Attribute
+    url(r'^product/$', ProductAttributeList.as_view(),
+        name=namespace_prefix + "product.list"),
+    url(r'^product/view/(?P<pk>\d+)/$', ProductAttributeDetails.as_view(),
+        name=namespace_prefix + "product.detail"),
+    url(r'^product/create/', ProductAttributeCreate.as_view(),
+        name=namespace_prefix + "product.create"),
+    url(r'^product/update/(?P<pk>\d+)/$', ProductAttributeUpdate.as_view(),
+        name=namespace_prefix + "product.update"),
+    url(r'^product/delete/(?P<pk>\d+)/$', ProductAttributeDestroy.as_view(),
+        name=namespace_prefix + "product.destroy"),
 ]
