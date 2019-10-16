431f89c9fad07dcb9614bac295ef189708a200a9
Ivan Chan <ivan@ivanplex.com>
2019-10-16 17:36:06 +0100

    Complete script for creating GS-8-18_M6_M6

--- shop/atomic/models.py
+++ shop/atomic/models.py
@@ -44,6 +44,8 @@ class AtomicPrerequisite(Prerequisite):
 
 
 class AtomicSpecification(Specification):
+    selected_component = models.ForeignKey(AtomicComponent,  on_delete=models.PROTECT, related_name='using',
+                                         null=True)
     atomic_prereq = models.ForeignKey(AtomicPrerequisite, on_delete=models.PROTECT, related_name='build_with',
                                       null=False)
 
---
+++ shop/atomic/test/API/test_API_AtomicGroup.py
@@ -0,0 +1,10 @@
+# import json
+# from rest_framework.test import APITestCase
+# from rest_framework import status
+#
+# from shop.atomic.models import AtomicComponent
+# from shop.atomic.serializers import AtomicComponentSerializer
+#
+#
+# class AtomicGroupTests(APITestCase):
+#     def setUp(self):
